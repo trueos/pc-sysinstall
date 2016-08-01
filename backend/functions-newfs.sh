ad8ec8dfde0f46164a91c60f0f83e4be855228db
ericbsd <ericturgeon.bsd@gmail.com>
2016-08-01 10:37:38 -0300

    Change geli for GhostBSD

--- backend/functions-newfs.sh
+++ backend/functions-newfs.sh
@@ -135,13 +135,21 @@ setup_filesystems()
 
       if [ -e "${PARTDIR}-enc/${PART}-encpass" ] ; then
 	# Using a passphrase
-        rc_halt "geli init -g -b -J ${PARTDIR}-enc/${PART}-encpass ${PARTDEV}"
+		if [ "${INSTALLTYPE}" = "GhostBSD" ] ; then
+		  rc_halt "geli init -b -J ${PARTDIR}-enc/${PART}-encpass ${PARTDEV}"
+		else
+          rc_halt "geli init -g -b -J ${PARTDIR}-enc/${PART}-encpass ${PARTDEV}"
+        fi
         rc_halt "geli attach -j ${PARTDIR}-enc/${PART}-encpass ${PARTDEV}"
 	touch ${TMPDIR}/.grub-install-geli
       else
 	# No Encryption password, use key file
         rc_halt "dd if=/dev/random of=${GELIKEYDIR}/${PART}.key bs=64 count=1"
-        rc_halt "geli init -g -b -s 4096 -P -K ${GELIKEYDIR}/${PART}.key ${PARTDEV}"
+        if [ "${INSTALLTYPE}" = "GhostBSD" ] ; then
+          rc_halt "geli init -b -s 4096 -P -K ${GELIKEYDIR}/${PART}.key ${PARTDEV}"
+        else
+          rc_halt "geli init -g -b -s 4096 -P -K ${GELIKEYDIR}/${PART}.key ${PARTDEV}"
+        fi
         rc_halt "geli attach -p -k ${GELIKEYDIR}/${PART}.key ${PARTDEV}"
 
       fi
@@ -157,7 +165,11 @@ setup_filesystems()
        for gC in $GELI_CLONE_ZFS_DISKS
        do
          echo_log "Setting up GELI on mirrored disks: ${gC}"
-         rc_halt "geli init -g -b -J ${PARTDIR}-enc/${PART}-encpass ${gC}"
+         if [ "${INSTALLTYPE}" = "GhostBSD" ] ; then
+           rc_halt "geli init -b -J ${PARTDIR}-enc/${PART}-encpass ${gC}"
+         else
+           rc_halt "geli init -g -b -J ${PARTDIR}-enc/${PART}-encpass ${gC}"
+         fi
          rc_halt "geli attach -j ${PARTDIR}-enc/${PART}-encpass ${gC}"
        done
     fi
