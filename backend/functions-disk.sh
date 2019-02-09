83e46f648769d72fe4ad2fc7d4a90c52cf687b84
ericbsd <ericturgeon.bsd@gmail.com>
2019-02-09 13:03:31 -0400

    Adding supports to 30 gpt partition

--- backend/functions-disk.sh
+++ backend/functions-disk.sh
@@ -526,7 +526,7 @@ setup_disk_slice()
             run_gpart_slice "${DISK}" "${BMANAGER}" "${s}"
             ;;
 
-          p1|p2|p3|p4|p5|p6|p7|p8|p9|p10|p11|p12|p13|p14|p15|p16|p17|p18|p19|p20|p21|p22|p23|p24|p25|p26|p27)
+          p1|p2|p3|p4|p5|p6|p7|p8|p9|p10|p11|p12|p13|p14|p15|p16|p17|p18|p19|p20|p21|p22|p23|p24|p25|p26|p27|p28|p29|p30)
             tmpSLICE="${DISK}${PTYPE}" 
             # Get the number of the gpt partition we are working on
             s="`echo ${PTYPE} | cut -c 2-3`"
